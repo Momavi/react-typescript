58c3ec2f732392a4808ef77ad05224672ebf3408
Momavi <68790545+Momavi@users.noreply.github.com>
2021-07-28 20:43:22 +0300

    0.95g
    gitpages add

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import store from './redux/redux-store';
 
 
 ReactDOM.render(
-	<BrowserRouter>
+	<HashRouter>
 		<Provider store={store}>
 			<App />
 		</Provider>
-	</BrowserRouter>, document.getElementById('root')
+	</HashRouter>, document.getElementById('root')
 );
 
